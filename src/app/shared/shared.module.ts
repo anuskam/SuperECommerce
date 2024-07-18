b9435491b6e6fb108af7049f8a1d399b1d30ae7c
Anna Moreta <anuskam92@gmail.com>
2024-07-18 08:37:47 +0200

    create login button and welcome text

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,15 @@ import { UrlImageFormatPipe } from './pipes/url-image-format.pipe';
 import { GoBackDirective } from './directives/go-back.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AngularMaterialsModule } from './utils/angular-materials/angular-materials.module';
 @NgModule({
-  declarations: [GoBackDirective, UrlImageFormatPipe, HeaderComponent, FooterComponent],
-  imports: [CommonModule],
-  exports: [GoBackDirective, UrlImageFormatPipe],
+  declarations: [
+    GoBackDirective,
+    UrlImageFormatPipe,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule, AngularMaterialsModule],
+  exports: [GoBackDirective, UrlImageFormatPipe, HeaderComponent],
 })
 export class SharedModule {}
--- src/app/shared/utils/angular-materials/angular-materials.module.ts
+++ src/app/shared/utils/angular-materials/angular-materials.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
-  providers: [
-    provideAnimationsAsync(),
-  ],
-  exports: []
+  imports: [CommonModule, MatMenuModule, MatButtonModule, MatIconModule],
+  providers: [provideAnimationsAsync()],
+  exports: [MatMenuModule, MatButtonModule, MatIconModule],
 })
-export class AngularMaterialsModule { }
+export class AngularMaterialsModule {}
