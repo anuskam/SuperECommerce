790969fcee04d9047f46197e1101bc2f6168a4c9
Anna Moreta <anuskam92@gmail.com>
2024-07-22 13:20:06 +0200

    fix guards

--- src/app/features/private/private-routing.module.ts
+++ src/app/features/private/private-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { adminGuard } from '../../core/guards/admin.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: 'admin-panel',
+        canMatch: [adminGuard],
         loadChildren: () =>
           import('./admin/admin.module').then(module => module.AdminModule),
       },
