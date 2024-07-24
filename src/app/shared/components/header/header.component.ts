940e04f7a7332f9a9d6403c11086d36b6d730261
Anna Moreta <anuskam92@gmail.com>
2024-07-24 12:42:50 +0200

    trying responsive nav

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,8 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  // private router = inject(Router);
-  /* login(): void {
-    this.router.navigate(['login']);
+  /* isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const nav = document.querySelector('.header__nav') as HTMLElement;
+    if (nav) {
+      if (this.isMenuOpen) {
+        nav.classList.add('open');
+      } else {
+        nav.classList.remove('open');
+      }
+    }
   } */
+  toggleMenu() {
+    const x = document.getElementById('myLinks');
+    if (x) {
+      if (x.style.display === 'block') {
+        x.style.display = 'none';
+      } else {
+        x.style.display = 'block';
+      }
+    }
+  }
 }
